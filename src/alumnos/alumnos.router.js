94fd1a3dc461eb7d5e5e97871654ac141471fbad
elancha <estherlancha@gmail.com>
2023-06-20 17:55:59 +0200

    Update alumnos.router.js

--- src/alumnos/alumnos.router.js
+++ src/alumnos/alumnos.router.js
@@ -2,6 +2,6 @@ const { Router } = require('maikuro');
 const alumnosController = require('./alumnos.controller')
 const router = Router();
 
-router.post('/alumnos/alumno', alumnosController.alumno); //Metodo post para hacer login
+router.post('/alumnos/alumno', alumnosController.alumno);
 
 module.exports = router;
